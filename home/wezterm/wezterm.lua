c266f9f22c2cf2c6cc29560b774d5d6c1719d3e5
24X7 <ChrisJRizzuto@outlook.com>
2026-09-05 15:41:06 -0400

    Adjust WezTerm theme and window appearance

--- home/wezterm/wezterm.lua
+++ home/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
 local is_macos = wezterm.target_triple:lower():find("darwin") ~= nil
 
-config.color_scheme = "rose-pine-moon"
+config.color_scheme = "Abernathy"
 config.max_fps = 120
 config.font = wezterm.font("Hack Nerd Font", { weight = "DemiBold" })
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
@@ -17,6 +17,10 @@ config.inactive_pane_hsb = {
   brightness = 0.5,
 }
 
+-- config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
 if is_windows then
   config.win32_system_backdrop = "Acrylic"
   config.window_background_opacity = 0.8
@@ -27,7 +31,7 @@ if is_macos then
   config.window_background_opacity = 0.85
   config.macos_window_background_blur = 80
   config.font_size = 15.0
-  config.window_frame.font_size = 13.0
+  -- config.window_frame.font_size = 13.0
 end
 
 return config
